40eec898ba886eba0f6469b8f5652c6ff47bcad7
Jhoan Dominguez <jhoandominguezu@gmail.com>
2021-09-12 22:39:10 -0500

    new design and dll corrections

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-require_once "logica/categoria.php";
-require_once "logica/producto.php";
 
   $opcion = 'default';
 
@@ -35,50 +33,62 @@ require_once "logica/producto.php";
 </head>
 <body>
     
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="./index.php">SPORT-SP</a>
-        
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" onClick="cambiarPagina" href="./index.php">Home</a>
-            </li>
-            <li class="nav-item dropdown">
-
-              <select class="nav-link dropdown-toggle" style="border: none" id="opcion">
-              <option value="default" <?php if($opcion=='default') echo "selected"?> > Opciones </option>
-              <option value="insertar" <?php if($opcion=='insertar') echo "selected"?> >Insertar</option>
-              <option value="consultar" <?php if($opcion=='consultar') echo "selected"?> >Consultar</option>
-							</select>
-              
-            </li>
-          </ul>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Pronto Muebles</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Nuestros Proveedores</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categorias
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li><a class="dropdown-item" href="#">Muebles</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Accesorios de Interiores</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <button class="btn btn-outline-success" type="submit">Iniciar Sesion</button>
+          <button class="btn btn-outline-success" type="submit">Crear Cuenta</button>
+        </form>
       </div>
-    </nav>
-
-    <div class="div-body">
-      <h1 style=" font-family: 'Brush Script MT', cursive; ">Bienvenido a SPORT SHOP</h1>
-
-      <?php
-      if ($pid != "" ) {
-          include $pid;
-      } else {
-      if ($opcion && $opcion=='default') {
-        ?>
-        <div class="body-imagen"></div>
-        <?php
-       }
-      else if ($opcion && $opcion=='insertar'){
-        include "./presentacion/insert.php";
-      }
-      else if ($opcion && $opcion=='consultar'){
-        include "./presentacion/read.php";
-      }
-    }?>
-      
     </div>
+  </nav>
+
+  <div class="div-body">
+    <h1 style=" font-family: 'Brush Script MT', cursive; ">Bienvenido a Pronto-Muebles</h1>
+
+    <?php
+    if ($pid != "" ) {
+        include $pid;
+    } else {
+    if ($opcion && $opcion=='default') {
+      ?>
+      <div class="body-imagen"></div>
+      <?php
+      }
+    else if ($opcion && $opcion=='insertar'){
+      include "./presentacion/insert.php";
+    }
+    else if ($opcion && $opcion=='consultar'){
+      include "./presentacion/read.php";
+    }
+  }?>
+  </div>
 
 </body>
 </html>
